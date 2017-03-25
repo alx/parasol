119142056c69376b15227647b4b44cca51e8bbef
Alexandre Girard <git@alexgirard.com>
2017-03-25 09:39:03 +0100

    v0.0.4 - dynamic legend, fetch graph object to manipulate it, begin to add filters on nodes/edges

---
+++ src/Legend.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem } from 'material-ui/List';
+
+import {
+  teal500,
+  amber500,
+  cyan500,
+} from 'material-ui/styles/colors';
+
+const COLORS = [teal500, amber500, cyan500];
+
+export default class Legend extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    const network = this.props.network;
+
+    if(!network || !network.graph)
+      return null
+
+    const legendItems = network.graph.nodes.map( node => {
+        return node.category;
+      }).filter( (category, index, self) => {
+        return self.indexOf(category) === index;
+      }).map( (category, index) => {
+        return <ListItem
+            key={index}
+            disabled={true}
+            leftAvatar={<Avatar size={30} backgroundColor={COLORS[index]}/>}
+          >{category}</ListItem>;
+      });
+
+    if(legendItems.length == 0)
+      return null;
+
+    return <List>
+      <ListItem
+        primaryText="Legend"
+        primaryTogglesNestedList={true}
+        initiallyOpen={true}
+        nestedItems={legendItems}
+      />
+    </List>
+
+  }
+}
