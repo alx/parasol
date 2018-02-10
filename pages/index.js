80af88a19a4ce39a56cc59f2023c847aaf34bb14
Alexandre Girard <git@alexgirard.com>
2018-02-10 17:44:28 +0100

    continue on nextjs integration

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,42 @@
-import dynamic from 'next/dynamic'
-import React from "react"
-import AppState from "../src/AppState"
+import dynamic from "next/dynamic";
+import React from "react";
+import AppState from "../src/AppState";
 
-import Meta from "../nextjs/components/meta"
+import Meta from "../nextjs/components/meta";
 
 const ParasolBundle = dynamic({
   modules: props => {
     const components = {
-      Parasol: import('../src/App')
-    }
-    return components
+      Parasol: import("../src/App")
+    };
+    return components;
   },
   render: (props, { Parasol }) => {
     const appState = new AppState();
     appState.initSettings(props.settings);
-    return <Parasol appState={appState} />
+    return <Parasol appState={appState} />;
   },
   ssr: false
-})
+});
 
-export default () => {
-  return <div>
-    <Meta />
-    <ParasolBundle settings={{
-      "networks": [
-        {
-          "url": "/static/les-miserables.json",
-          "name": "Les Miserables",
-          "options": {
-            "layout": "forcelink",
-            "loader": {
-              "name": "json",
-              "options": {
-              "limitEdgeCount": 500
-              }
-            }
-          }
-        }
-      ]
-    }}/>
-  </div>
-}
+const Index = props => {
+  return (
+    <div>
+      <Meta />
+      <ParasolBundle settings={props.settings} />
+    </div>
+  );
+};
+
+Index.componentDidMount = async function() {
+  const res = await fetch("/static/settings/json");
+  const data = await res.json();
+
+  console.log(`settings.json fetched`);
+
+  return {
+    settings: data
+  };
+};
+
+export default Index;
